a878c53db5584599fd6b13ddd11e8f7451ee909b
Frederic Lamsens <frederic.lamsens@ugent.be>
2025-08-06 17:33:19 +0200

    refactor: database client init

--- app/_server/data-sources/postgres-db-gcnd/index.ts
+++ app/_server/data-sources/postgres-db-gcnd/index.ts
@@ -1,30 +1,5 @@
 import {KnexPgAdapter, createDataSource} from '@kottster/server';
 import knex from 'knex';
-import {DataSourceType} from "@kottster/common";
-
-// const dataSource = createDataSource({
-//     type:  DataSourceType.postgres, // string value "postgres" is invalid
-//     name: 'postgres-db-gcnd',
-//     version: '1.0.0',
-//     init: () => {
-//         const client = knex({
-//             client: 'pg',
-//             connection: {
-//                 host: process.env.DB_HOST || 'localhost',
-//                 port: Number(process.env.DB_PORT) || 5432,
-//                 user: process.env.DB_USER || 'gcnd',
-//                 password: process.env.DB_PASSWORD || 'gcnd',
-//                 database: process.env.DB_NAME || 'gcnd',
-//             },
-//             searchPath: ['public'],
-//         });
-//
-//         return new KnexPgAdapter(client);
-//     },
-//     tablesConfig: {}
-// });
-//
-// export default dataSource;
 
 const client = knex({
     client: 'pg',
